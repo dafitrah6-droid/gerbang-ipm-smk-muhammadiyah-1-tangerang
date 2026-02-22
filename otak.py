56058650731b4586ef8b976632ccbbeec412c328
dafitrah6-droid <dafitrah6@gmail.com>
2026-02-22 23:15:10 +0700

    Update otak.py

--- otak.py
+++ otak.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 app.secret_key = 'ipm_smkm1_tgr_luxury_2026_dafitrah_ultimate'
 
 # --- CONFIGURATION DATABASE (Hanya bagian ini yang disesuaikan agar tidak Error 500) ---
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/ipm_data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:[YOUR-PASSWORD]@db.agwcwoulwactpmtruevs.supabase.co:5432/postgres'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
@@ -812,4 +812,5 @@ def piagam():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
 
