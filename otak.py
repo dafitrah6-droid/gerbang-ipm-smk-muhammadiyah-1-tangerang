531971dae2573295349f2b43e2dbcc7f58ebbaf1
dafitrah6-droid <dafitrah6@gmail.com>
2026-02-24 13:12:13 +0700

    Update otak.py

--- otak.py
+++ otak.py
@@ -14,9 +14,10 @@ app = Flask(__name__)
 app.secret_key = 'ipm_smkm1_tgr_luxury_2026_dafitrah_ultimate'
 
 # --- CONFIGURATION DATABASE (Hanya bagian ini yang disesuaikan agar tidak Error 500) ---
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:[ewhbfwybedijnwqiybdibindebwydbiwqbdbewybdu]@db.agwcwoulwactpmtruevs.supabase.co:5432/postgres'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/ipm_data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
 # --- DATABASE MODELS ---
 
 class User(db.Model):
@@ -89,7 +90,6 @@ UI_CORE_HEADER = """
 <html lang="id">
 <head>
     <meta charset="UTF-8">
-    <meta name="google-site-verification" content="YcLkbqpi--metg2W9OqS5B6SoF8jsqp3L_HLs1CIvss" />
     <title>IPM PORTAL | SMKM 1 TGR</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
@@ -811,9 +811,3 @@ def piagam():
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
-
-
-
